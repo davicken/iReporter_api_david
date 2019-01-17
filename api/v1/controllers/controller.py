8940aee0d4a5c874bb2502e3b8868783376100ac
davicken <mwesigwadavidk@gmail.com>
2019-01-17 23:26:12 +0300

    adds create redflag endpoint

--- api/v1/controllers/controller.py
+++ api/v1/controllers/controller.py
@@ -1,4 +1,13 @@
+# validate  location input (should be a string)
 def validate_string(r):
     if not isinstance(r,str):
         raise ValueError
     return 'Location is not a string'
+
+# def required_image_field(img):
+#     if len(img) == 0:
+#         return 'You should attach an image evidance'
+
+# def comment_length(comm):
+#     if len(comm) > 4:
+#         return 'Please enter at most 4 characters'
--- api/v1/models/redflags.py
+++ api/v1/models/redflags.py
@@ -1,15 +1,21 @@
-class RedFlag:
-    def __init__(self, f_id, createdBy, location, status, comment):
-        self.f_id = f_id
-        self.createdBy = createdBy
+
+class Incident:
+    def __init__(self, inc_id, location, createdOn, createdBy, status, comment, images, videos):
+        self.inc_id = inc_id
         self.location = location
-        self.status = status
+        self.createdOn = createdOn
+        self.createdBy = createdBy
+        self.images = images
+        self.videos = videos
         self.comment = comment
+        self.status = status
+
+class RedFlag(Incident):
+    def __init__(self, inc_type, inc_id, location, createdOn, createdBy, status, comment, images, videos):
+        self.inc_type = inc_type
+        super().__init__(inc_id, location, createdOn, createdBy, status, comment, images, videos)
+
     def to_json(self):
-        return {'f_id' : self.f_id, 'createdBy' : self.createdBy, 'location': self.location, 'status': self.status, 'comment': self.comment}   
+        return {'inc_id' : self.inc_id, 'inc_type' : self.inc_type, 'createdOn' : self.createdOn, 'createdBy' : self.createdBy, 'location': self.location,'status': self.status, 'comment': self.comment, 'images': self.images, 'videos': self.videos}   
 
 
-
-    
-
-    
