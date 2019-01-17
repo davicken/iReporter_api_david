6dae39c807eaa01f44c75aee987888f67e022957
davicken <mwesigwadavidk@gmail.com>
2019-01-18 01:36:23 +0300

    adds get all red-flags endpoint

--- api/v1/views/views.py
+++ api/v1/views/views.py
@@ -45,36 +45,16 @@ def create_redflag():
         return jsonify({'status': 400, 'message': 'location should be a string'})
     return jsonify({'status': 201, 'redflags': [flag_record.to_json()]})
 
-# get all red flags
-# @app.route('/api/v1/redflags', methods=['GET'])
-# def get_all_redflags():
-#     json_redflags = []  
-#     print(redflags)
-#     for redflag in redflags:
-#         json_redflags.append(redflag.to_json())
+# get all red-flags end-point
+@app.route('/api/v1/redflags', methods=['GET'])
+def get_all_redflags():
+    json_redflags = []  
+    print(redflags)
+    for redflag in redflags:
+        json_redflags.append(redflag.to_json())
 
-#     print(json_redflags)
-#     print(len(json_redflags))
-#     if len(json_redflags) < 1:
-#         return jsonify({ 'status' : 400, 'message': 'There are no red-flag records'})
-#     return jsonify({'status' : 200, 'redflags': json_redflags})
-
-
-# edit a specific red flag record
-# @app.route('/api/v1/redflags/<int:f_id>', methods=['PATCH'])
-# def edit_redflag(f_id):
-#         data = request.get_json()
-#         redflags=[]
-#         for redflag in redflags:
-#               redflags.append(redflag.to_json())
-#         redflag =[redflag for redflag in redflags if redflag['f_id']==f_id]
-#         redflag[0]['f_id']= data['f_id']
-#         redflag[0]['createdBy']= data['createdBy']
-#         redflag[0]['location']= data['location']
-#         redflag[0]['status']= data['status']
-#         redflag[0]['comment']= data['comment']
-#         if len(redflags) < 1:
-#             return jsonify({ 'status' : 400, 'message': 'There are no red-flag records'})
-#         return jsonify({'status' : 200, 'redflag': redflag[0]})
-    
-    
+    print(json_redflags)
+    print(len(json_redflags))
+    if len(json_redflags) < 1:
+        return jsonify({ 'status' : 400, 'message': 'There are no red-flag records'})
+    return jsonify({'status' : 200, 'redflags': json_redflags})
